b461ab56c58c972ea5199d7a144dbcce45cd2844
Louie Jay Cantores <louiejaycantores@gmail.com>
2023-05-08 22:41:08 +0800

    adding some name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,14 +31,14 @@
 
                         @if ($messages->u_id !== auth()->id())
                             <div class="d-flex justify-content-start flex-column">
-                                <span class="fw-lighter ps-1 text-light user-name-font">{{$messages->first_name}} {{$messages->last_name}}</span>
+                                <span class="fw-lighter ps-1 text-dark user-name-font">{{$messages->first_name}} {{$messages->last_name}}</span>
                                 <p class="bg-primary px-3 py-1 rounded text-light other-user"> {{$messages->message}}</p>
                             </div> 
 
                         @else
                             <div class="d-flex justify-content-end">
                                 <div class="logged-user">
-                                    <span class="fw-lighter ps-1 text-light user-name-font">{{$messages->first_name}} {{$messages->last_name}}</span>
+                                    <span class="fw-lighter ps-1 text-dark user-name-font">{{$messages->first_name}} {{$messages->last_name}}</span>
                                     <p class="bg-primary px-3 py-1 rounded text-light">{{$messages->message}}</p>
                                 </div>
                             </div>
