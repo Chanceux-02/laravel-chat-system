33774f51f5f698c08249bed8d76b31b15777ac7d
Louie Jay Cantores <louiejaycantores@gmail.com>
2023-05-17 15:41:25 +0800

    adding delete message feature for one on one chat

--- resources/views/ajax/singleChat.blade.php
+++ resources/views/ajax/singleChat.blade.php
@@ -12,9 +12,10 @@
                 <div class="d-flex flex-column justify-content-end">
                     <span class="fw-lighter ps-1 text-dark text-end user-name-font">{{$messages->first_name}} {{$messages->last_name}}</span>
                     <div class="d-flex">
-                        <form action="" method="post">
+                        <form action="{{route('delete-message')}}" method="post">
                             @csrf
                             @method('DELETE')
+                            <input type="hidden" name="messageId" value="{{$messages->m_id}}">
                             <button type="submit" name="delMessage" class="btn btn-unstyled p-0"><i class="fa-solid fa-trash-can fa-2xs"></i></button>
                         </form>
                         <p class="bg-primary ms-1 px-3 py-1 rounded text-light">{{$messages->message}}</p>
