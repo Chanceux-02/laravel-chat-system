3751965a5491305ff2d94af2771c919472aa571c
Louie Jay Cantores <louiejaycantores@gmail.com>
2023-05-17 14:52:22 +0800

    adding delete profile feature

--- app/Http/Controllers/DestroyController.php
+++ app/Http/Controllers/DestroyController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DestroyController extends Controller
 {
-    //
+    public function destroyProfile(){
+        $userId = auth()->id();
+        $user = User::findOrFail($userId);
+        $check = $user->delete();
+        if (!$check) {
+            return redirect()->back()->withErrors($check);
+        }
+        return redirect('/login')->with(['message' => 'deleted successful']);
+    }
 }
